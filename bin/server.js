d19abf283c5d96d8beff4962f85456f6d950308d
Alexander Wesolowski <foglerek@gmail.com>
2016-08-10 20:41:29 -0700

    Change everything

---
+++ bin/server.js
@@ -0,0 +1,27 @@
+import config from '../config'
+import server from '../server/main'
+import _debug from 'debug'
+import socketio from 'socket.io'
+import http from 'http'
+
+const debug = _debug('app:bin:server')
+const port = config.server_port
+const host = config.server_host
+
+let socketserver = http.Server(server.callback()),
+    io = socketio(socketserver);
+
+let state = {
+    selected_characters: 0
+}
+
+io.on('connection', (socket) => {
+    socket.on('select_character', () => {
+        state.selected_characters++
+        io.emit('state_changed', state)
+    })
+})
+
+socketserver.listen(port)
+debug(`Server is now running at http://${host}:${port}.`)
+debug(`Server accessible via localhost:${port} if you are using the project defaults.`)
